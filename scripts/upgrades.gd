a783255f5efb931cae4b8986a897c3996ba79ce3
Jade <167011818+Jade-Origami@users.noreply.github.com>
2026-07-05 19:08:54 +0100

    balance changes

--- scripts/upgrades.gd
+++ scripts/upgrades.gd
@@ -6,7 +6,7 @@ var upgrades = [
 		"pretty_text": "Ghost Writer",
 		"explanation": "Automatically types the next letter in a double-letter group and +3 mult when it does",
 		"id": "double_letters_bypass",
-		"price": 5,
+		"price": 4,
 		"trigger_time": "on_type",
 		"weight": 10
 	},
@@ -14,7 +14,7 @@ var upgrades = [
 		"pretty_text": "Higher Wages",
 		"explanation": "+2 coins at end of round",
 		"id": "flat_coin_increase",
-		"price": 4,
+		"price": 3,
 		"trigger_time": "reward",
 		"weight": 20
 	},
@@ -22,7 +22,7 @@ var upgrades = [
 		"pretty_text": "Longer Deadlines",
 		"explanation": "Increase timer max by 1/4",
 		"id": "round_time_increase",
-		"price": 5,
+		"price": 4,
 		"trigger_time": "sentence_start",
 		"weight": 20
 	},
@@ -30,7 +30,7 @@ var upgrades = [
 		"pretty_text": "Autocorrect",
 		"explanation": "decrease the time that the timer is sped up by after a mistake by 1/4",
 		"id": "mistake_penalty",
-		"price": 8,
+		"price": 6,
 		"trigger_time": "mistake_made",
 		"weight": 15
 	},
@@ -38,7 +38,7 @@ var upgrades = [
 		"pretty_text": "Editor's Note",
 		"explanation": "+2 discards every round",
 		"id": "reroll_sentence",
-		"price": 3,
+		"price": 2,
 		"trigger_time": "round_start",
 		"weight": 20
 	},
@@ -46,7 +46,7 @@ var upgrades = [
 		"pretty_text": "Predictive Text",
 		"explanation": "Press Tab to automatically fill in the current word once per round",
 		"id": "tab_to_fill",
-		"price": 11,
+		"price": 6,
 		"trigger_time": "round_start",
 		"weight": 10
 	},
@@ -54,15 +54,15 @@ var upgrades = [
 		"pretty_text": "Ace Writer",
 		"explanation": "Every typed \"A\" gives +2 mult and +2 base",
 		"id": "a_upgrade",
-		"price": 6,
+		"price": 5,
 		"trigger_time": "on_type",
-		"weight": 5
+		"weight": 6
 	},
 	{
 		"pretty_text": "Sublime Text",
 		"explanation": "Every typed \"S\" gives x1.25 mult",
 		"id": "s_upgrade",
-		"price": 7,
+		"price": 6,
 		"trigger_time": "on_type",
 		"weight": 5
 	},
@@ -70,7 +70,7 @@ var upgrades = [
 		"pretty_text": "Zippy Keys",
 		"explanation": "every typed \"Z\" gives x2 mult ",
 		"id": "z_upgrade",
-		"price": 10,
+		"price": 4,
 		"trigger_time": "on_type",
 		"weight": 10
 	},
@@ -86,7 +86,7 @@ var upgrades = [
 		"pretty_text": "Upgrade Stencil",
 		"explanation": "x1.5 mult for every empty upgrade slot (this included)",
 		"id": "empty_slot",
-		"price": 7,
+		"price": 6,
 		"trigger_time": "sentence_end",
 		"weight": 8
 	},
@@ -110,7 +110,7 @@ var upgrades = [
 		"pretty_text": "Surplus Items",
 		"explanation": "+1 upgrade available in shop \n[cannot be copied]",
 		"id": "shop_surplus",
-		"price": 10,
+		"price": 6,
 		"trigger_time": "shop",
 		"weight": 15
 	},
@@ -118,7 +118,7 @@ var upgrades = [
 		"pretty_text": "Valued Customer",
 		"explanation": "Rerolls start at 0",
 		"id": "low_reroll",
-		"price": 6,
+		"price": 5,
 		"trigger_time": "shop",
 		"weight": 14
 	},
@@ -126,7 +126,7 @@ var upgrades = [
 		"pretty_text": "Mistake Fanatic",
 		"explanation": "Typed mistakes still advance the sentence, but are not counted as typed",
 		"id": "ignore_mistakes",
-		"price": 7,
+		"price": 5,
 		"trigger_time": "mistake_made",
 		"weight": 10
 	},
