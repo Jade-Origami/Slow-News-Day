fe1b0c835fa362975c6494ab28b86b2b73304b66
Jade <167011818+Jade-Origami@users.noreply.github.com>
2026-07-15 11:33:00 +0100

    long anticipated!
    - you can now see what upgrades do when they are in your score panel.
    you do this by hovering over them

--- scripts/upgrades_panel.gd
+++ scripts/upgrades_panel.gd
@@ -200,3 +200,24 @@ func Item_4_Up() -> void:
 	active_upgrades[2] = temp
 	whole_panel_update()
 	_on_item_3_pressed()
+
+
+func _on_item_1_mouse_entered() -> void:
+	$Rotate_Item_1/Item.text = active_upgrades[0].explanation
+func _on_item_1_mouse_exited() -> void:
+	$Rotate_Item_1/Item.text = active_upgrades[0].pretty_text
+
+func _on_item_2_mouse_entered() -> void:
+	$Rotate_Item_2/Item.text = active_upgrades[1].explanation
+func _on_item_2_mouse_exited() -> void:
+	$Rotate_Item_2/Item.text = active_upgrades[1].pretty_text
+
+func _on_item_3_mouse_entered() -> void:
+	$Rotate_Item_3/Item.text = active_upgrades[2].explanation
+func _on_item_3_mouse_exited() -> void:
+	$Rotate_Item_3/Item.text = active_upgrades[2].pretty_text
+
+func _on_item_4_mouse_entered() -> void:
+	$Rotate_Item_4/Item.text = active_upgrades[3].explanation
+func _on_item_4_mouse_exited() -> void:
+	$Rotate_Item_4/Item.text = active_upgrades[3].pretty_text
