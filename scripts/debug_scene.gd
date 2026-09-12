b48c9a5528f935534ab47abe5735283c3ad31de0
Jade <167011818+Jade-Origami@users.noreply.github.com>
2026-09-12 19:24:47 +0100

    New Fonts + Convert Main Scene to Binary file
    to make it load faster when running, the encryption factor is an unfortunate side effect

--- scripts/debug_scene.gd
+++ scripts/debug_scene.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
 func _on_gamescreen_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/gameplay_holder.tscn")
+	get_tree().change_scene_to_file("res://scenes/gameplay_holder.scn")
